15b787672c0f422a28244d19aea12ba46d59ea49
rajatchopraa <rajatsanjaychopra@gmail.com>
2025-09-18 00:28:23 +0530

    second way of using rest template

--- src/main/java/com/rajat1/restapi/service/EmployeeService.java
+++ src/main/java/com/rajat1/restapi/service/EmployeeService.java
@@ -2,6 +2,7 @@ package com.rajat1.restapi.service;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -19,10 +20,12 @@ public class EmployeeService {
 	private ModelMapper mapper;
 	@Autowired
 	private RestTemplate restTemplate;
+	@Value("${departmentservice.base.url}")
+	private String departmentServiceBaseUrl;
 	public EmployeeResponse getEmployeeById(int id) {
 		Employee employee = repo.findById(id).get();
 		EmployeeResponse response = mapper.map(employee, EmployeeResponse.class);
-		DepartmentResponse departmentResponse = restTemplate.getForObject("http://localhost:8083/department/{id}", DepartmentResponse.class, id);
+		DepartmentResponse departmentResponse = restTemplate.getForObject(departmentServiceBaseUrl+"/{id}", DepartmentResponse.class, id);
 		response.setDepartmentResponse(departmentResponse);
 		return response;
 	}
